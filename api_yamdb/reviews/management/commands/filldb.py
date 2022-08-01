bb56496473cf66f21a2e62fbe87bd189018891dc
Ольга Воронюк <helga-61rus@yandex.ru>
2022-08-01 23:58:15 +0300

    после 2 ревью

--- api_yamdb/reviews/management/commands/filldb.py
+++ api_yamdb/reviews/management/commands/filldb.py
@@ -7,7 +7,7 @@ from django.core.management.base import BaseCommand
 from reviews.models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
-DATA_DIR = os.path.join(settings.STATICFILES_DIRS[0], 'data')
+DATA_DIR = os.path.join(settings.STATIC_ROOT, 'data')
 
 
 class Command(BaseCommand):
